318c18707382ce905864d9d7c39ef7624cf96614
Jiaqi Li <lijiaqigreat@gmail.com>
2015-08-10 01:09:04 -0400

    95% finish gulp
    Haven't tested js

--- gulpfile.js
+++ gulpfile.js
@@ -1,37 +1,69 @@
 var gulp = require('gulp');
 var path = require('path');
 
-var less = require('gulp-less-sourcemap');
+var less = require('gulp-less');
 var shell = require('gulp-shell');
 var babel = require('gulp-babel');
+var mustache = require('gulp-mustache');
+var uncss = require('gulp-uncss');
+var ext_replace=require('gulp-ext-replace');
+var sourcemaps = require('gulp-sourcemaps');
+var uglify = require('gulp-uglify');
+
+var browserSync = require("browser-sync").create();
+var reload=browserSync.reload;
 
 gulp.task('shrinkwrap', shell.task('npm shrinkwrap'));
 
 gulp.task('less', function () { return gulp.src('./css/main.less')
+  .pipe(sourcemaps.init())
   .pipe(less({
     paths: [ path.join(__dirname, 'node_modules','bootstrap', 'less') ],
-    sourceMap: {
-      sourceMapFileInline: false
-    }
   }))
-  .pipe(uncss({
-    html: ['index.debug.html']
-  }))
-  .pipe(gulp.dest('./css/'));
+  .pipe(sourcemaps.write('./'))
+  .pipe(gulp.dest('css/'));
 });
 gulp.task('html',function(){ return gulp.src('index.mustache')
-    .pipe(mustache({
-      ext: ""
-    }))
-    .pipe(gulp.dest("index.debug.html"));
+  .pipe(mustache({
+    ext: ""
+  }))
+  .pipe(ext_replace('.debug.html'))
+  .pipe(gulp.dest("./"));
 });
-gulp.task('js',function(){
-  return gulp.src('js/main.js')
-    .pipe(babel({
-      sourceMaps: true
-    }))
-    .pipe(gulp.dest('js/main.min.js'));
+gulp.task('js',function(){ return gulp.src('js/main.js')
+  .pipe(ext_replace('.min.js'))
+  .pipe(sourcemaps.init())
+  .pipe(babel())
+  .pipe(uglify())
+  .pipe(sourcemaps.write('./'))
+  .pipe(gulp.dest('js/'));
 });
-
 gulp.task('build',['less', 'html', 'js']);
 
+gulp.task('html2', function(){ return gulp.src('index.mustache')
+  .pipe(mustache({
+    ext: ".min"
+  }))
+  // .pipe(html_minify())
+  .pipe(ext_replace('.html'))
+  .pipe(gulp.dest("./"));
+});
+gulp.task('less2', ['html2'], function () { return gulp.src('./css/main.css')
+  .pipe(uncss({
+    html: ['index.html']
+  }))
+  .pipe(ext_replace('.min.css'))
+  .pipe(gulp.dest("./"));
+});
+gulp.task('build2',['less2', 'html2', 'js']);
+gulp.task('serve', ['build'], function () {
+  browserSync.init({
+    server: {
+      baseDir: "./",
+      index: "index.debug.html"
+    }
+  });
+  gulp.watch(['css/**/*.less'],['less',reload]);
+  gulp.watch(['index.mustache','partials/**'], reload);
+  gulp.watch(['js/**'],['js',reload]);
+});
---
+++ js/main.js
@@ -0,0 +1,2 @@
+import * as math from "./test.js";
+alert("2π = " + math.sum(math.pi, math.pi));
