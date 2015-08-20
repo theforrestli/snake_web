b5f7fe42511b44efdb760062f3bf6af4b0a4cf5c
Jiaqi Li <lijiaqigreat@gmail.com>
2015-08-19 23:25:11 -0400

    WIP last WIP

--- js/test.js
+++ js/test.js
@@ -5,10 +5,22 @@ Object.defineProperty(exports, "__esModule", {
 });
 exports.sum = sum;
 
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
 function sum(a, b) {
   return a + b;
 }
 
 var pi = 3.14;
 exports.pi = pi;
+var svg = SVG("main");
+var rect = svg.rect(100, 100).attr({ fill: '#f06' });
+
+var App = function App(url, id) {
+  _classCallCheck(this, App);
+
+  this.svg = SVG(id);
+  this.status = "initializing";
+  this.rest = {};
+};
 //# sourceMappingURL=test.js.map
--- js6/test.js
+++ js6/test.js
@@ -2,3 +2,12 @@ export function sum(a,b){
   return a+b;
 }
 export var pi=3.14;
+var svg=SVG("main");
+var rect = svg.rect(100, 100).attr({ fill: '#f06'})
+class App {
+  constructor(url,id){
+    this.svg=SVG(id);
+    this.status="initializing";
+    this.rest={};
+  }
+}
