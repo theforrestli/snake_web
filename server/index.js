9902f290f980a4efa5ee0fe5922c060abb959e9b
Jiaqi Li <lijiaqigreat@gmail.com>
2015-11-22 23:10:45 -0500

    Have a base design for server.
    - use `room` for game communication
    - make serve be generic game server

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ var socket_io = require('socket.io');
 var prefix = "/";
 var gen = function(json,id){
   var io = socket_io(id);
-  var game = new Game(json),
+  var game = new Game(json);
   io.on("connection",(socket) => {
     socket.emit("init", game.json);
     socket.on("reload",(message) =>{
@@ -21,16 +21,22 @@ var gen = function(json,id){
     game,
     tick
   }
-}
+};
+var message_valid = () => true;
 
 var io = socket_io();
 
 io.on("connection",(socket) => {
-  console.log("connected");
+  console.log(`connected: ${socket.id}`);
+  socket.join("default");
   global.socket=socket;
-  socket.on("test",(message) => {
-    console.log(message);
-    global.message = message;
+  socket.on("m",(m) => {
+    console.log("m");
+    console.log(m);
+    if(!message_valid(m)){
+      return;
+    }
+    socket.to("default").emit(m[0],[socket.id,m[1]])
   })
 })
 
