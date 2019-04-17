524828a9424e267c62b2d99df53580e7e57c7b0a
Jiaqi Li <lijiaqigreat@gmail.com>
2019-04-17 00:13:52 -0700

    Use commands instead.

--- js6/conn.js
+++ js6/conn.js
@@ -7,8 +7,8 @@ const map = require('map');
 const blobToBuffer = require('blob_to_buffer')
 const protobuf = require("protobufjs");
 //TODO
-const CommandP = $.get("https://raw.githubusercontent.com/lijiaqigreat/personal_server/master/protobuf/command.proto").then(protoFile => {
-  return protobuf.parse(protoFile).root.lookup("Command");
+const CommandsP = $.get("https://raw.githubusercontent.com/lijiaqigreat/personal_server/master/protobuf/command.proto").then(protoFile => {
+  return protobuf.parse(protoFile).root.lookup("Commands");
 });
 
 const keyMap = {
@@ -25,18 +25,21 @@ module.exports = function(id, url){
     version: 1,
   }
   const ws = new WebSocket(url);
-  // setInterval(() => {console.log("tick")}, 500);
   ws.onopen = async () => {
     const game = new Game(map(param).game);
-    const Command = await CommandP;
+    window.game = game;
+    const Commands = await CommandsP;
     var inited = 0;
     ws.onmessage = (event) => {
       new Promise((resolve, reject) => {
         blobToBuffer(event.data, resolve);
       }).then(buffer => {
-        const c = Command.decode(buffer);
-        // console.log("c");
-        game.handleCommand2(c);
+        const cs = Commands.decode(buffer).commands;
+        const time1 = new Date();
+        for(var t=0;t<cs.length;t++){
+          game.handleCommand2(cs[t]);
+        }
+        const time2 = new Date();
         if(inited != "initialized") {
           clearTimeout(inited);
           inited = setTimeout(() => {
@@ -54,8 +57,6 @@ module.exports = function(id, url){
         }
       });
     }
-    //TODO
-    window.game = game;
 
   };
   return ws;
